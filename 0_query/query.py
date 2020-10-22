ee16630d3b2be8ecf1939864b21b184fc8356ae3
sudarshan <vijays@fysik.dtu.dk>
2020-10-22 13:38:49 +0200

    Updates on query

--- 0_query/query.py
+++ 0_query/query.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc700eaa43a9a78fa1b83c825e009250517f22aa2e157587e3f6b121d6bf7d9d
-size 688
+oid sha256:05edd900f52de2d752ffeb674c0f962086968660d7e0c09a912952b2f188c5ec
+size 917
